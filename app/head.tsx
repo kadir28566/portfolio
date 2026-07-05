61c4ecf2beb5fbb42c30c9d5aa253fcb60d5219a
Abdülkadir Yılmaz <kadir28566@gmail.com>
2026-07-06 02:58:11 +0300

    favico eklendi

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,9 @@ export const metadata: Metadata = {
     "yazılım mühendisi",
     "sistem programlama",
   ],
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
